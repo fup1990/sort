a53bafc846b0d131f1df1f947ab301cd2f55804a
fup1990 <fup1990@163.com>
2018-08-07 11:23:53 +0800

    quick sort

--- src/main/java/com/gome/sort/quick/QuickSort.java
+++ src/main/java/com/gome/sort/quick/QuickSort.java
@@ -2,8 +2,42 @@ package com.gome.sort.quick;
 
 public class QuickSort {
 
-    public void queckSort(int[] array) {
-        int median = getMedian(array[0], array[array.length / 2], array[array.length - 1]);
+    public void queckSort(int[] array, int left, int right) {
+        if (left < right) {
+            int median = getMedian(array[left], array[(left + right) / 2], array[right]);
+            // 将中数交换到数组的最后
+            if (median == array[left]) {
+                array[left] = array[right];
+                array[right] = median;
+            } else if (median == array[(left + right) / 2]) {
+                array[(left + right) / 2] = array[right];
+                array[right] = median;
+            }
+            // 比较
+            int i = left;
+            int j = right - 1;
+            while (true) {
+                while (array[i] < median) {
+                    i++;
+                }
+                while (array[j] > median) {
+                    j--;
+                }
+                if (i < j) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                } else {
+                    // 枢纽元与i位置的元素交换
+                    array[right] = array[i];
+                    array[i] = median;
+                    break;
+                }
+            }
+            // 递归
+            queckSort(array, left, i - 1);
+            queckSort(array, i + 1, right);
+        }
     }
 
     /**
@@ -38,9 +72,12 @@ public class QuickSort {
     }
 
     public static void main(String[] args) {
+        int[] array = {3,9,20,5,1,87,46,17,4};
         QuickSort sort = new QuickSort();
-        int median = sort.getMedian(2, 23, 9);
-        System.out.println(median);
+        sort.queckSort(array, 0, array.length - 1);
+        for (int i : array) {
+            System.out.println(i);
+        }
     }
 
 }
